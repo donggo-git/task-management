cbb4eb8d1daec2275b2caf60a63287e051c1d7a2
Dong Nguyen <dlttthvlhhth@gmail.com>
2025-11-27 18:00:53 -0800

    implement update user information controller and route path for upload image

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -85,7 +85,29 @@ const getUserProfile = async (req, res) => {
 }
 
 const updateUserProfile = async (req, res) => {
-    try { }
+    try {
+        const user = await User.findById(req.user.id)
+        if (!user)
+            return res.status(401).json({ message: "User not found" })
+
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(req.body.password, salt)
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            role: updatedUser.role,
+            token: generateToken(updatedUser._id)
+        })
+    }
     catch (error) {
         res.status(500).json({ message: "Server error", error: error.message })
     }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,4 +7,14 @@ router.post("/login", loginUser);
 router.get("profile", protect, getUserProfile);
 router.put("/profile", protect, updateUserProfile);
 
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file)
+        return res.status(400).json({ message: "No file uploaded" })
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`
+
+    req.status(200).json({
+        imageUrl
+    })
+})
+
 module.exports = router;
